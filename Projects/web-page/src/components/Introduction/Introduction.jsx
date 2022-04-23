9c0d4d2b94a27b64341d4da384c01e209cad671d
Ozan <ozan28061@gmail.com>
2022-04-23 23:30:25 +0200

    keyframes

--- Projects/web-page/src/components/Introduction/Introduction.jsx
+++ Projects/web-page/src/components/Introduction/Introduction.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import './Introduction.css';
+import React from "react";
+import "./Introduction.css";
 const Introduction = () => {
   return (
-    <div> 
-        <div className="intro">
-            <div className="intro-left">
-                <div className="intro-left-wrapper">
-                    <h2 className="name-intro">Hello, My name is</h2>
-                    <h1 className="name">Ozan Eski</h1>
-                    <div className="title">
-                        <div className="title-wrapper">
-                            <div className="title-item">React</div>
-                            <div className="title-item">React-Native</div>
-                            <div className="title-item">JavaScript</div>
-                            <div className="title-item">Django</div>
-                            <div className="title-item">Python</div>
-                        </div>
-                    </div>
-                </div>
+    <div>
+      <div className="intro">
+        <div className="intro-left">
+          <div className="intro-left-wrapper">
+            <h2 className="name-intro">Hello, My name is</h2>
+            <h1 className="name">Ozan Eski</h1>
+            <div className="title">
+              <div className="title-wrapper">
+                <div className="title-item">React</div>
+                <div className="title-item">React-Native</div>
+                <div className="title-item">JavaScript</div>
+                <div className="title-item">Django</div>
+                <div className="title-item">Python</div>
+              </div>
             </div>
-            <div className="intro-right">Right</div>
+            <div className="intro-description">
+              I am a curious front-end developer, interested in learning new
+              skills, and skilled at developing solutions.
+            </div>
+          </div>
         </div>
-
+        <div className="intro-right">Right</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Introduction
+export default Introduction;
